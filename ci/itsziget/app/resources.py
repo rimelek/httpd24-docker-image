9b3e4815439b8b0d5212dc31f8da811f20f7f6a4
Takács Ákos <rimelek@rimelek.hu>
2019-12-26 14:01:30 +0100

    add a common argument parser base class

--- ci/itsziget/app/resources.py
+++ ci/itsziget/app/resources.py
@@ -6,7 +6,7 @@ PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__ + "/../../.."))
 BUILD_DIR = PROJECT_ROOT + "/.build"
 
 
-class BuildArgumentParser(argparse.ArgumentParser):
+class CommonArgumentParser(argparse.ArgumentParser):
 
     def __init__(self, default_build_number=None):
         super().__init__(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -16,14 +16,8 @@ class BuildArgumentParser(argparse.ArgumentParser):
                           help="VCS tag")
         self.add_argument("-b", "--branch",
                           help="VCS branch")
-        self.add_argument("-T", "--docker-start-timeout",
-                          help="How many seconds can the tester wait to start the container", type=int, default=180)
         self.add_argument("-i", "--image-name",
                           help="Desired name of the Docker image without version tag", required=True)
-        self.add_argument("-s", "--skip-test",
-                          help="Skip the test of the built image", action="store_true")
-        self.add_argument("-r", "--repository-alias",
-                          help="Remote repository alias", default="origin")
         self.add_argument("-B", "--build-number",
                           help="A unique build number to distinguish between different builds."
                                "Git commit hash can be a good idea.",
@@ -31,3 +25,18 @@ class BuildArgumentParser(argparse.ArgumentParser):
         self.add_argument("-e", "--event-type",
                           help="The type of CI event.", choices=["push", "api", "cron"], required=True)
 
+
+class BuildArgumentParser(CommonArgumentParser):
+
+    def __init__(self, default_build_number=None):
+        super().__init__(default_build_number)
+        self.add_argument("-T", "--docker-start-timeout",
+                          help="How many seconds can the tester wait to start the container", type=int, default=180)
+        self.add_argument("-s", "--skip-test",
+                          help="Skip the test of the built image", action="store_true")
+        self.add_argument("-r", "--repository-alias",
+                          help="Remote repository alias", default="origin")
+
+
+class DeployArgumentParser(CommonArgumentParser):
+    pass
