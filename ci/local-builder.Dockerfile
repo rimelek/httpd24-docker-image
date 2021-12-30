28f6efdd89cef8f99aa181a06adb194045507cc4
Takács Ákos <rimelek@rimelek.hu>
2021-12-31 00:04:28 +0100

    support copying code into the build container

--- ci/local-builder.Dockerfile
+++ ci/local-builder.Dockerfile
@@ -1,5 +1,9 @@
-FROM cimg/python:3.7
+FROM cimg/python:3.7 as base
 
-COPY --chown=circleci:circleci requirements.txt /requirements.txt
+COPY --chown=circleci:circleci ci/requirements.txt /requirements.txt
 
 RUN pip install -r /requirements.txt
+
+ARG WORKDIR=/app
+COPY --chown=circleci:circleci . "$WORKDIR"
+WORKDIR "$WORKDIR"
--- ci/local-builder.sh
+++ ci/local-builder.sh
@@ -2,11 +2,34 @@
 
 set -eu -o pipefail
 
+if [[ -z "${COPY_MODE+x}" ]]; then
+  COPY_MODE=0
+fi
+
+compose=(docker-compose -f local-builder.yml)
+
 (
   cd "$(dirname "$0")"
 
   workdir="$(cd .. && pwd)"
 
-  docker-compose -f local-builder.yml build
-  docker-compose -f local-builder.yml run --rm -v "$workdir:$workdir" --workdir "$workdir" ci "$@"
+  "${compose[@]}" build --build-arg "WORKDIR=$workdir"
+
+  echo "COPY_MODE: $COPY_MODE"
+
+  if [[ "$COPY_MODE" != "1" ]]; then
+    export WORKDIR="$workdir"
+    compose+=(-f local-builder.volumes.yml)
+  fi
+
+  "${compose[@]}" up -d
+  set +e
+  "${compose[@]}" exec ci "$@"
+  err=$?
+  set -e
+  "${compose[@]}" down
+  if [[ "$err" != "0" ]]; then
+    >&2 echo "Build failed"
+    exit 1
+  fi
 )
