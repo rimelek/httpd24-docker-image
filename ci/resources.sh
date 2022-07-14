af9bb37db0bfade47ea96966f898eaf9894ad2ca
Ákos Takács <takacs.akos@it-sziget.hu>
2022-07-15 00:52:35 +0200

    fix undefined CUSTOM_TAGS

--- ci/resources.sh
+++ ci/resources.sh
@@ -123,7 +123,7 @@ deployCommandGen () (
     local LATEST_VERSION
     local LATEST_MINOR
     local LATEST_MAJOR
-    local CUSTOM_TAGS
+    local CUSTOM_TAGS=""
     local IMAGE_NAME
     local IMAGE_TAG="${GIT_HASH}"
     local OPTIND
